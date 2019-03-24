776962541937df61c489d955e7389d6b8e1e663b
Simon Gibson <terryteo@gmail.com>
2019-03-25 06:37:49 +1300

    ex1 and ex2 done.

---
+++ ex2.rb
@@ -0,0 +1,9 @@
+# A comment, this is so you can read your programme later.
+# Anything after the # is ignored by ruby.
+
+puts "I could have code like this." # and after the comment is ignored.
+
+# You can also use a comment to 'disable' or comment out a piece of code:
+# puts "This wont run."
+
+puts "This will run."
