418ddc1b580de87d5f9e076a058ad4eb04a33e00
Simon Gibson <terryteo@gmail.com>
2019-03-27 06:56:46 +1300

    exercise 5.

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Zed A. Beeblebrock'
+my_age = 456
+my_height = 183 # centimeters
+my_weight = 94 # kgs
+my_eyes = 'Silver'
+my_teeth = 'fanged'
+my_hair = 'Green'
+
+puts "Lets talk about #{my_name}."
+puts "He's #{my_height} centimeters tall."
+puts "He's #{my_weight} kgs heavy."
+puts "Actually he is a bit overweight."
+puts "He's got #{my_hair} hair and #{my_eyes} eyes."
+puts "His teeth are strikingly #{my_teeth}."
+
+#this line is tricky
+puts "If we add his age - #{my_age}, height - #{my_height} and weight - #{my_weight}, we get #{my_age + my_height + my_weight}."
