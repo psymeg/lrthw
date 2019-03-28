b4fa0c6baf364a79550b2b5ff3c20778b5bc87ef
Simon Gibson <terryteo@gmail.com>
2019-03-29 06:09:50 +1300

    ex5a and ex6

---
+++ ex5a.rb
@@ -0,0 +1,17 @@
+name = 'Zed A. Beeblebrock'
+age = 456
+height = 183 # centimeters
+weight = 94 # kgs
+eyes = 'Silver'
+teeth = 'fanged'
+hair = 'Green'
+
+puts "Lets talk about #{name}."
+puts "He's #{height} centimeters tall."
+puts "He's #{weight} kgs heavy."
+puts "Actually he is a bit overweight."
+puts "He's got #{hair} hair and #{eyes} eyes."
+puts "His teeth are strikingly #{teeth}."
+
+#this line is tricky
+puts "If we add his age - #{age}, height - #{height} and weight - #{weight}, we get #{age + height + weight}."
