e79cf1d49f1b7f5fcb4307a14c509daccd9d66f4
Simon Gibson <terryteo@gmail.com>
2019-03-31 17:11:47 +1300

    moar exercies

---
+++ ex9.rb
@@ -0,0 +1,14 @@
+#heres some new strange stuff, remember to type it exactly.
+#
+days = "mon tue wed thu fri sat sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+puts %q{
+  There's something going on here.
+  With this weird quote
+  We'll be able to type as much as we like.
+  Even four lines if we want, or 6, or 8.
+}
+ 
